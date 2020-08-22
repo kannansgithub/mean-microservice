640978a48aa4eb2e60801af79059d107bad6bb5b
Kannan S <kannan-inbox@outlook.com>
2020-08-22 10:20:12 +0530

    Compleated docker and Skaffold

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,12 +7,16 @@ app.use(bodyParser.json());
 const events = [];
 app.post("/events", async (req, res) => {
   const event = req.body;
+  console.log(`Event Received- ${event}`);
   events.push(event);
   await axios.post("http://posts-clusterip-srv:4000/events", event);
+  console.log(`Event Sent posts-clusterip-srv`);
   await axios.post("http://comments-srv:4001/events", event);
+  console.log(`Event Sent- comments-srv`);
   await axios.post("http://query-srv:4002/events", event);
+  console.log(`Event Sent- query-srv`);
   await axios.post("http://moderation-srv:4003/events", event);
-
+  console.log(`Event Sent- moderation-srv`);
   res.send({ status: "OK" });
 });
 app.get("/events", (req, res) => {
--- posts/index.js
+++ posts/index.js
@@ -13,13 +13,14 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = {
     id,
     title,
   };
+  console.log('Post Created Successfully and Calling Event');
   await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
@@ -27,14 +28,17 @@ app.post("/posts", async (req, res) => {
       title,
     },
   });
+  console.log('Event Called Successfully');
   res.status(201).send(posts[id]);
 });
 
 app.post("/events", (req, res) => {
+  console.log('Event Received');
   console.log(req.body.type);
   res.send();
 });
 
 app.listen(4000, () => {
+    console.log('v1');
   console.log("Listening on PORT 4000");
 });
